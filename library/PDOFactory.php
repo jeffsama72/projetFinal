d6b10a9edc1844bb07528de5db8c3cb4fae50ddc
jeffsama72 <fouassier.jeanfrancois72@gmail.com>
2017-03-31 04:04:12 -0400

    pdo

--- library/PDOFactory.php
+++ library/PDOFactory.php
@@ -4,7 +4,7 @@ class PDOFactory
 {
   public static function getMysqlConnection()
   {
-   	$db = new PDO('mysql:host=localhost;dbname=projetimie', 'root', '');		// On changera le nop de la bdd et tout ( a faire )
+   	$db = new PDO('mysql:host=localhost;dbname=projetimie', 'root', 'Guillaume72');		// On changera le nop de la bdd et tout ( a faire )
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->exec("SET NAMES 'utf8';");
 
